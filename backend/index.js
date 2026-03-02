90d4769b57a13bcb5b50d8218e92c18c10381331
Sonal Kumar <jhasonal9142@gmail.com>
2026-03-02 19:09:39 +0530

    Final Changes

--- backend/index.js
+++ backend/index.js
@@ -65,6 +65,7 @@ message:"Email and password are required fields all fields are required to login
     if(!user){
         return res.status(400).json({
             success:false,
+            // message:"User is not exist with this email, please signup first"
             message:"User is not exist with this email, please signup first"
         })
     }
