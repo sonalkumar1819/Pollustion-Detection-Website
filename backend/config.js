0c677d6de668181944c8f2774785507462351268
Sonal Kumar <jhasonal9142@gmail.com>
2026-03-06 18:34:23 +0530

    Final Changes

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose');
 const mongoos=mongoose.connect("mongodb://localhost:27017/pollusatdata");
 if(mongoos){
-    console.log("connected to database successfully IN MONGO");
+    
+    console.log("Connected to MongoDB successfully in backend");
 }
 module.exports=mongoose;
