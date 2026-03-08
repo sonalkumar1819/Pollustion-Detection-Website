1dcfd1fc19138439b60d8785879cc1782423ab9d
Sonal Kumar <jhasonal9142@gmail.com>
2026-03-08 17:22:35 +0530

    Final Changes

--- sonalr/space-web-main/src/NotFound.jsx
+++ sonalr/space-web-main/src/NotFound.jsx
@@ -6,12 +6,12 @@ function NotFound(){
             <h1 style={{fontSize:'48px', color:'red'}}>404 Error</h1><br></br>
             <h1 style={{fontSize:'48px', color:'orange'}}>Page Not Found</h1>
             <h1 style={{fontSize:'48px', color:'yellow'}}>😢</h1>
-            <h1>style={{fontSize:'48px', color:'green'}}>But don't worry, you can find your way back home!</h1>
+            <h1 style={{fontSize:'48px', color:'green'}}>But don't worry, you can find your way back home!</h1>
             <br></br>
             <br>
             </br>
             <button>
-                <Link to="/" style={{textDecoration:'none', color:'white'}}>Go to Home</Link>
+                <Link to="/" style={{textDecoration:'none', color:'white'}}>Home</Link>
 
             </button>
         </div>
