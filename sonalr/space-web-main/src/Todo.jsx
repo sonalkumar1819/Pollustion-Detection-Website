f33d0d85e0885f5a993eb822402bd0bbc9e95c34
Sonal Kumar <jhasonal9142@gmail.com>
2026-03-30 19:02:37 +0530

    first commit

--- sonalr/space-web-main/src/Todo.jsx
+++ sonalr/space-web-main/src/Todo.jsx
@@ -1,79 +1,79 @@
-// import React, { useState } from 'react'
+import React, { useState } from 'react'
 
-// // Task component
-// function Todo() {
-// //   let[todos,setTodos]=useState([]);
-// //   let[title,setTitle]=useState("");
-// //   const[body,setBody]=useState("");
-// //   const[editingIdx,setEditingIdx]=useState("");
-// //   const handleButton=()=>{
-// //     if(!title.trim() || !body.trim())return;
+// Task component
+function Todo() {
+//   let[todos,setTodos]=useState([]);
+//   let[title,setTitle]=useState("");
+//   const[body,setBody]=useState("");
+//   const[editingIdx,setEditingIdx]=useState("");
+//   const handleButton=()=>{
+//     if(!title.trim() || !body.trim())return;
 
-// //     if(editingIdx!=null){
-// //       const updateTodos=[...todos];
-// //       updateTodos[editingIdx]={title,body};
-// //       setTodos(updateTodos);
-// //       setEditingIdx(null);
-// //     }
-// //     else{
-// //       setTodos([...todos,{title,body}]);
-// //     }
+//     if(editingIdx!=null){
+//       const updateTodos=[...todos];
+//       updateTodos[editingIdx]={title,body};
+//       setTodos(updateTodos);
+//       setEditingIdx(null);
+//     }
+//     else{
+//       setTodos([...todos,{title,body}]);
+//     }
 
-// //     setTitle("");
-// //     setBody("");
-// //   };
-// //   const handleEdit=(index)=>{
-// //     setTitle(todos[index].title);
-// //     setBody(todos[index].body);
-// //     setEditingIdx(index);
-// //   }
-// //   const handleDelete=(index)=>{
-// //     const filtered=todos.filter((_,i));
-// //     setTodos(filtered);
-// //   }
+//     setTitle("");
+//     setBody("");
+//   };
+//   const handleEdit=(index)=>{
+//     setTitle(todos[index].title);
+//     setBody(todos[index].body);
+//     setEditingIdx(index);
+//   }
+//   const handleDelete=(index)=>{
+//     const filtered=todos.filter((_,i));
+//     setTodos(filtered);
+//   }
 
-// const[todos,setTodos]=useState([]);
-// const [title,setTitle]=useState("");
-// const[body,setBody]=useState("");
+const[todos,setTodos]=useState([]);
+const [title,setTitle]=useState("");
+const[body,setBody]=useState("");
 
-// const handleAdd=()=>{
-//   if(!title.trim() || !body.trim())return;
-//   setTodos([...todos,{title,body}]);
-//   setTitle("");
-//   setBody("");
-// };
-// const handleCheck=(id)=>{
-//     const update=todos.map((todo)=>{
-//         todo.id === id ? {...todo, completed: }
-//     })
-// }
+const handleAdd=()=>{
+  if(!title.trim() || !body.trim())return;
+  setTodos([...todos,{title,body}]);
+  setTitle("");
+  setBody("");
+};
+const handleCheck=(id)=>{
+    const update=todos.map((todo)=>{
+        todo.id === id ? {...todo, completed: }
+    })
+}
 
-//   return (
-//     <>
-//             <input type="text" placeholder ="title" value={title} onChange={(e)=>setTitle(e.target.value)} disabled/>
-//             <textarea placeholder="Body" value={body} onChange={(e)=>setBody(e.target.value)}/>
-//                 {
-//                     todos.map((todo)=>{
-//                         <div key={todo.id}>
-//                             <input type ="checkbox" checked={todo.completed} onChange={{()=>handleCheck(todo.id)}} />
-//                         </div>
-//                     })
-//                 }
-//             <button onClick={handleAdd}>Add Todos</button>
+  return (
+    <>
+            <input type="text" placeholder ="title" value={title} onChange={(e)=>setTitle(e.target.value)} disabled/>
+            <textarea placeholder="Body" value={body} onChange={(e)=>setBody(e.target.value)}/>
+                {
+                    todos.map((todo)=>{
+                        <div key={todo.id}>
+                            <input type ="checkbox" checked={todo.completed} onChange={{()=>handleCheck(todo.id)}} />
+                        </div>
+                    })
+                }
+            <button onClick={handleAdd}>Add Todos</button>
 
            
-//             <div>
-//             {todos.length===0 && <p>No Todods Yet</p>}
-//             {todos.map((todo,index)=>(
-//               <div key={index} >
-//               <h2>{todo.title}</h2>
-//               <p>{todo.body}</p>
-//               </div>
-//             ))}
-//             </div>
+            <div>
+            {todos.length===0 && <p>No Todods Yet</p>}
+            {todos.map((todo,index)=>(
+              <div key={index} >
+              <h2>{todo.title}</h2>
+              <p>{todo.body}</p>
+              </div>
+            ))}
+            </div>
             
-//     </>
-//   )
-// };
+    </>
+  )
+};
 
-// export default Todo;
+export default Todo;
